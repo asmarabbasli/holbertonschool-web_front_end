a0650e5522c8b092c7fdc0581be8e9dd14492542
Your Name <you@example.com>
2025-11-01 19:11:26 +0400

    new

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -3,14 +3,26 @@ function setCookies() {
   const email = document.getElementById('email').value;
 
   const expirationDate = new Date();
-  expirationDate.setDate(expirationDate.getDate() + 10); // 10 gün əlavə et
+  expirationDate.setDate(expirationDate.getDate() + 10);
 
   document.cookie = `firstname=${firstname}; expires=${expirationDate.toUTCString()}; path=/`;
   document.cookie = `email=${email}; expires=${expirationDate.toUTCString()}; path=/`;
 }
 
+function getCookie(name) {
+  const cookies = document.cookie.split(';');
+  for (let cookie of cookies) {
+    const [key, value] = cookie.trim().split('=');
+    if (key === name) return value;
+  }
+  return '';
+}
+
 function showCookies() {
+  const firstname = getCookie('firstname');
+  const email = getCookie('email');
+
   const p = document.createElement('p');
-  p.textContent = `Cookies: ${document.cookie}`;
+  p.textContent = `Email: ${email} - Firstname: ${firstname}`;
   document.body.appendChild(p);
 }
